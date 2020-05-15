be14563a7905bb34089edf8cc5948301502c22dc
abhinav <abhinav.eerlapati@mckesson.com>
2020-05-15 01:15:56 -0700

    added Docker build file and create project for test

---
+++ src/test/java/com/training/project102/controller/WelcomeControllerTest.java
@@ -0,0 +1,13 @@
+package com.training.project102.controller;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class WelcomeControllerTest{
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+	
+}
